0c34b09928a2e4d23a4731f494bde4614cd0c8c3
dev-chulnuji <dev.chulbuji@gmail.com>
2017-08-01 20:39:44 +0900

    set network config  - OkHttp logging interceptor  - Gson converter  - RxJava2Adapter

---
+++ app/src/main/java/com/showdesk/util/net/ShowDeskRetrofit.java
@@ -0,0 +1,46 @@
+package com.showdesk.util.net;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by ladmusician.kim on 01/08/2017.
+ */
+
+public class ShowDeskRetrofit {
+
+    public static final String BASE_URL = "https://";
+    private static Retrofit sRetrofit;
+
+    private ShowDeskRetrofit() {
+    }
+
+    private static class ShowDeskRetrofitLazyHolder {
+        static final ShowDeskRetrofit INSTANCE = new ShowDeskRetrofit();
+    }
+
+    public static ShowDeskRetrofit getInstance() {
+        return ShowDeskRetrofitLazyHolder.INSTANCE;
+    }
+
+    public static void init() {
+        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+
+        logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
+        httpClientBuilder.addInterceptor(logging);
+        OkHttpClient client = httpClientBuilder.build();
+
+        if (sRetrofit == null) {
+            sRetrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .client(client)
+                    .build();
+        }
+    }
+}
